b5422462feb8a106141c2ed58b56f8f36958a88d
Bicok7 <159533025+Bicok7@users.noreply.github.com>
2025-02-15 20:19:51 +0700

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:shaikhanas@gmail.com">MieRollers1@gmail.com</a>
+         <a href="mailto:anasbhai@gmail.com">AdminRollers@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>08:00am to 10:00pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="https://www.google.com/maps/place/Jl.+Rawa+Lindung+No.4,+RT.2%2FRW.4,+Cilangkap,+Kec.+Cipayung,+Kota+Jakarta+Timur,+Daerah+Khusus+Ibukota+Jakarta+13870/@-6.3269006,106.9046776,17z/data=!3m1!4b1!4m6!3m5!1s0x2e69ed31def012b1:0xd6ca8f3db5c74f5f!8m2!3d-6.3269006!4d106.9046776!16s%2Fg%2F11fpj4wxpr?entry=ttu&g_ep=EgoyMDI1MDIxMi4wIKXMDSoJLDEwMjExNDU1SAFQAw%3D%3D">Jl Rawa Lindung 04, Cipayung, Jakarta Timur, DKI Jakarta, Indonesia</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:1234567890">123-456-7890</a>
+         <a href="tel:1112223333">111-222-3333</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Peods</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
